ae7a71a734ef6ae39e6159ef046a8155e80eaeab
Muhammed Al Qamary <154762118+MuhammedAlQamary@users.noreply.github.com>
2024-05-06 16:24:21 +0300

    adding models

---
+++ Price Comparison/Models/PriceHistory.cs
@@ -0,0 +1,27 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBtoclasses.Models;
+
+[Table("PriceHistory")]
+public partial class PriceHistory
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int ProdId { get; set; }
+
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime Date { get; set; }
+
+    [ForeignKey("ProdId")]
+    [InverseProperty("PriceHistories")]
+    public virtual Product Prod { get; set; }
+}
---
+++ Price Comparison/Models/ProductSponsored.cs
@@ -0,0 +1,29 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBtoclasses.Models;
+
+[Table("ProductSponsored")]
+public partial class ProductSponsored
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Cost { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime StartDate { get; set; }
+
+    public int Duration { get; set; }
+
+    public int ProdId { get; set; }
+
+    [ForeignKey("ProdId")]
+    [InverseProperty("ProductSponsoreds")]
+    public virtual ProductDetail Prod { get; set; }
+}
