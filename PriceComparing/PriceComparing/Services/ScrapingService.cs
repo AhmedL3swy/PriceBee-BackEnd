d690bb0d81baa973d65dce519a583ef80c503d3c
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-05-13 21:45:43 +0300

    add: client Service for scraping

---
+++ PriceComparing/PriceComparing/Services/ScrapingService.cs
@@ -0,0 +1,18 @@
+﻿namespace PriceComparing.Services
+{
+    public class ScrapingService
+    {
+        HttpClient client;
+        public ScrapingService(IHttpClientFactory httpClientFactory)
+        {
+            client = httpClientFactory.CreateClient("ScrapingClient");
+        }
+
+        public async Task<string> Get(string api , string url)
+        {
+
+            var response = await client.GetAsync($"/{api}/?url={url}");
+            return await response.Content.ReadAsStringAsync(); ;
+        }
+    }
+}
