9107a90840e9863ff29572ea2dcaec0adcde1903
abdullah-mohamed9 <121031158+abdullah-mohamed9@users.noreply.github.com>
2024-06-06 12:56:20 +0300

    APIs for Get, GetById, Post

---
+++ PriceComparing/PriceComparing/Controllers/ProductController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PriceComparing.UnitOfWork;
+using DTO;
+using DataAccess.Models;
+
+namespace PriceComparing.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		UnitOfWOrks _UnitOfWork;
+
+		public ProductController(UnitOfWOrks unitOfWork)
+		{
+			_UnitOfWork = unitOfWork;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllProducts()
+		{
+			var products = await _UnitOfWork.ProductRepository.SelectAll();
+			if (products == null) { return NotFound(); }
+			List<ProductDTO> productsDTO = new List<ProductDTO>();
+			foreach (var product in products)
+			{
+				productsDTO.Add(new ProductDTO()
+				{
+					Id = product.Id,
+					Name_Local = product.Name_Local,
+					Name_Global = product.Name_Global,
+					Description_Local = product.Description_Local,
+					Description_Global = product.Description_Global,
+					SubCategoryId = product.SubCategoryId,
+					BrandId = product.BrandId
+				});
+			}
+			return Ok(productsDTO);
+		}
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetProductById(int id)
+		{
+			var product = await _UnitOfWork.ProductRepository.SelectById(id);
+			if (product == null) { return NotFound(); }
+			ProductDTO productDTO = new ProductDTO()
+			{
+				Id = product.Id,
+				Name_Local = product.Name_Local,
+				Name_Global = product.Name_Global,
+				Description_Local = product.Description_Local,
+				Description_Global = product.Description_Global,
+				SubCategoryId = product.SubCategoryId,
+				BrandId = product.BrandId
+			};
+			return Ok(productDTO);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddProduct(ProductPostDTO productDTO)
+		{
+			if (productDTO == null) { return BadRequest(); }
+			Product product = new Product()
+			{
+				Name_Local = productDTO.Name_Local,
+				Name_Global = productDTO.Name_Global,
+				Description_Local = productDTO.Description_Local,
+				Description_Global = productDTO.Description_Global,
+				SubCategoryId = productDTO.SubCategoryId,
+				BrandId = productDTO.BrandId
+			};
+			await _UnitOfWork.ProductRepository.Add(product);
+			_UnitOfWork.savechanges();
+			return Ok(product);
+		}
+
+
+
+	}
+}
--- PriceComparing/PriceComparing/UnitOfWork/UnitOfWOrks.cs
+++ PriceComparing/PriceComparing/UnitOfWork/UnitOfWOrks.cs
@@ -7,41 +7,29 @@ namespace PriceComparing.UnitOfWork
     public class UnitOfWOrks
     {
 		DatabaseContext _db;
-        //WE Will change code Here After the Models is Done 
+		//WE Will change code Here After the Models is Done 
 
-         //GenericRepository<Student> studentsRepository;
-         //GenericRepository<Department> departmentRepository;
+		GenericRepository<Product> productRepository;
 
-        public UnitOfWOrks(DatabaseContext db)
+
+		public UnitOfWOrks(DatabaseContext db)
         {
             _db = db;
         }
 
-        //public GenericRepository<Student> StudentsRepository
-        //{
-        //    get
-        //    {
-        //        if (studentsRepository == null)
-        //        {
-        //            studentsRepository = new GenericRepository<Student>(_db);
+		public GenericRepository<Product> ProductRepository
+		{
+			get
+			{
+				if (productRepository == null)
+				{
+					productRepository = new GenericRepository<Product>(_db);
+				}
+				return productRepository;
+			}
+		}
 
-        //        }
-        //        return studentsRepository;
-        //    }
-        //}
-
-        //public GenericRepository<Department> DepartmentRepository
-        //{
-        //    get
-        //    {
-        //        if(departmentRepository == null)
-        //        {
-        //            departmentRepository = new GenericRepository<Department>(_db);
-        //        }
-        //        return departmentRepository;
-        //    }
-        //}
-        public void savechanges()
+		public void savechanges()
         {
             _db.SaveChanges();
         }
