0ed7f4bce5fff4e1503bb5bdab55f760dce01e51
AhmedL3swy <Ahmed.a.alesawy@gmail.com>
2024-06-07 17:26:21 +0300

    Add: AddProductController To Cotnain All Logic of Admin Adding Products

---
+++ PriceComparing/PriceComparing/Controllers/AddProductController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Models;
+using DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PriceComparing.UnitOfWork;
+
+namespace PriceComparing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddProductController : ControllerBase
+    {
+        UnitOfWOrks _UnitOfWork;
+
+        public AddProductController(UnitOfWOrks unitOfWork)
+        {
+            _UnitOfWork = unitOfWork;
+        }
+
+        [HttpPost("AddProduct")]
+        public async Task<IActionResult> AddProduct(AddProductDTO productDTO)
+        {
+           
+            // Add The Product
+            int productID = await _UnitOfWork.ProductRepo.Add(productDTO.ProductPostDTO);
+            // Append Product Details
+            await _UnitOfWork.ProductRepo.AppendProductDetails(productID, productDTO.ProductDetailDTO);
+            // Append Product Images
+            await _UnitOfWork.ProductRepo.AppendProductImages(productID, productDTO.ProductImageDTO);
+               
+             _UnitOfWork.savechanges();
+            return Ok(productID);
+        }
+    }
+}
