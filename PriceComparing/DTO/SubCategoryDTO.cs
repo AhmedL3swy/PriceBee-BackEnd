78cfab362760704b33c9e5745369b144f5e4b617
MuhammedAlQamary <Muhammed.AlQamary@f-eng.tanta.edu.eg>
2024-06-12 14:16:38 +0300

    fix category controller

--- PriceComparing/DTO/SubCategoryDTO.cs
+++ PriceComparing/DTO/SubCategoryDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DTO
 {
-    internal class SubCategoryDTO
+    public class SubCategoryDTO
     {
       public int id { get; set; }
         public string Name_Local { get; set; }
